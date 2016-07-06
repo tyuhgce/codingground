eec18ab8cb7d78a3abc7cb3aef593b0db7bf940e
tyuhgce <tyuhgce@gmail.com>
2016-07-06 07:56:57 +0600

    Created by Coding Ground Client

---
+++ New Project-20160706/LinkedListNode.cs
@@ -0,0 +1,21 @@
+﻿namespace DataWorks
+{
+    public sealed class LinkedListNode<T>
+    {
+        public LinkedListNode(T value)
+        {
+            Value = value;
+        }
+
+        public T Value { get; set; }
+        public LinkedListNode<T> Next { get; set; }
+        public LinkedListNode<T> Previous { get; set; }
+
+        internal void Invalidate()
+        {
+            Value = default(T);
+            Next = null;
+            Previous = null;
+        }
+    }
+}
